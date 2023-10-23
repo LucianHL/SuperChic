11c7a84d2a6ef5c112d711b088bcc4c9eab170b3
andriish <andriish@users.noreply.github.com>
2023-10-23 14:48:24 +0200

    Update mesint.f (#55)

--- src/mes/mesint.f
+++ src/mes/mesint.f
@@ -11,6 +11,12 @@ ccc   interpolates gg-->MM amplitude in cos(theta)
       endif
 
       if(i.eq.0)i=i+1
+      if(i.le.0) then
+        write(*,*) 'Error in mesint'
+        write(*,*) 'pol=',pol,'j=',j,'cost=',cost,'i=',i
+        write(*,*) 'cmain=',cmin,'cinc=',cinc
+        i=1
+      end if
 
       m=(mesamp(pol,2,j,i+1)-mesamp(pol,2,j,i))/cinc
       out=mesamp(pol,2,j,i)+m*(cost-mesamp(pol,1,j,i))
