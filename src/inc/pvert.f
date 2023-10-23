dd4ccaffbadb23b2acba248b008dc88cfdc0789b
andriish <andriish@users.noreply.github.com>
2023-10-23 15:09:30 +0200

    Update pvert.f (#59)

--- src/inc/pvert.f
+++ src/inc/pvert.f
@@ -1,2 +1,2 @@
-      integer barv(20,5,2),pdgv(20,5),statv(20,5),nvert
-      double precision momv(20,5,4),massv(20,5)
+      integer barv(20,10,2),pdgv(20,10),statv(20,10),nvert
+      double precision momv(20,10,4),massv(20,10)
