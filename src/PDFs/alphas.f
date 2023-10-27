bf1d864534ecf1a3189d0ae02abc24909fa120f4
andriish <andriish@users.noreply.github.com>
2023-10-27 11:58:10 +0200

    Update alphas.f

--- src/PDFs/alphas.f
+++ src/PDFs/alphas.f
@@ -1,8 +1,10 @@
 ccc   strong coupling constant, for PDFINPUT = LHAPDF
       function alphas(qsq)
       implicit double precision (a-z)
-
+      if (dsq.le.100.d0) then 
+      alphas=alphasPDF(10.0d0)
+      else
       alphas=alphasPDF(dsqrt(qsq))
-
+      end if
       return
       end
