4337087e54ce0695872caa8273275f7df32f079e
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-11-02 16:30:25 +0100

    Eliminate more compiler warnings

--- src/LbyL/xspenz.f
+++ src/LbyL/xspenz.f
@@ -48,6 +48,7 @@ C---------------------------------------------------------------------C
 C       20.07.83    UE 20.08.85                                       C
 CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 C       COMPLEX*16 XSPENZ,W,SUM,Z,U
+        INTEGER K,II
         COMPLEX*16 CSPEN ,W,SUM,Z,U
         REAL*8 RZ,AZ,A1
         REAL*8 B(9)
