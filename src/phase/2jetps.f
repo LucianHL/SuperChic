6930b210a9278af8f68c4a8c975081d397f3fe1f
andriish <andriish@users.noreply.github.com>
2023-11-03 16:38:32 +0100

    Update 2jetps.f

--- src/phase/2jetps.f
+++ src/phase/2jetps.f
@@ -13,6 +13,13 @@ ccc   generates two-body phase space for particles of equal mass mq
       include 'pi.f'
       include 'mom.f'
 
+      if ( 2d0*mq .GE. mx ) then
+      u=mx**2/4.0d0
+      t=mx**2/4.0d0
+      ps=0.0d0
+      return 
+      endif
+
 c      rphi=ran2()
 
 c      print*,rphi,rtheta
