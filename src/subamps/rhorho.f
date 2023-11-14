16eb5172099b0c466efb45e5fb04565b3966aca5
andriish <andriish@users.noreply.github.com>
2023-11-14 19:40:38 +0100

    Avoid divisions by explicit zero in rhorho.f (#122)
    * Avoid divisions by explicit zero in rhorho.f
    
    * Update rhorho.f
    
    ---------
    
    Co-authored-by: Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>

--- src/subamps/rhorho.f
+++ src/subamps/rhorho.f
@@ -49,22 +49,24 @@ ccc   gg --> rhorho subprocess amplitude
 
       call wfoctet(mx,2,a28,a28n)
       call wfoctet(mx,4,a48,a48n)
-
       out2=out2*(a28n/a28)
       out3=out3*(a28n/a28)**2
-
+      
+      if(dabs(a48n).gt.1d-4)then
       out4=out4*(a48n/a48)
       out5=out5*(a48n/a48)*(a28n/a28)
       out6=out6*(a48n/a48)*(a28n/a28)
       out7=out7*(a48n/a48)**2
-
+      endif
+      
       out2a=out2a*(a28n/a28)
       out3a=out3a*(a28n/a28)**2
-
+      if(dabs(a48n).gt.1d-4)then
       out4a=out4a*(a48n/a48)
       out5a=out5a*(a48n/a48)*(a28n/a28)
       out6a=out6a*(a48n/a48)*(a28n/a28)
       out7a=out7a*(a48n/a48)**2
+      endif
 
       if(dabs(a28n).lt.1d-4)then
          out2=0d0
