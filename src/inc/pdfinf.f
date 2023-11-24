0755daee5515c5c74565dacd42d2a0e413f0194c
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-11-24 14:42:28 +0100

    Inc

--- src/inc/pdfinf.f
+++ src/inc/pdfinf.f
@@ -1,4 +1,8 @@
       character*100 PDFname
       integer PDFmember
+      double precision pdfq2min
+      integer pdfwarn
       common/pdfint/PDFname
       common/pdfint1/PDFmember
+      common/pdfbounds/pdfq2min
+      common/pdfwarn/pdfwarn
