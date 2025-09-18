51763b09746f28fe7180292e680abc95033a58af
LucianHL <l.harland-lang@ucl.ac.uk>
2025-09-18 10:54:46 +0100

    Update pAinit.f

--- src/phase/pAinit.f
+++ src/phase/pAinit.f
@@ -14,9 +14,6 @@ ccc   re-initialise incoming nucleon momenta in nucleon-nucleon cms
       include 'sAA.f'
       include 'ion_inel.f'
 
-c      print*,ion_em
-c      stop
-
 ccccc Used for incoherent photon emission from AA
 
       saa=an**2*s
@@ -26,9 +23,6 @@ cccc   Using rigidity formula - ion energy per nucleon is Z/A x proton energy
 
       sp=si*(an/az)**2
 
-c      print*,si,dsqrt(si)
-c      stop
-
       beta=dsqrt(1d0-4d0*mp**2/sp)
       betaa=dsqrt(1d0-4d0*mp**2/sp*an**2/az**2)
 
@@ -37,17 +31,11 @@ c      stop
       betap=dsqrt(1d0-4d0*mp**2/spa)
       betaap=dsqrt(1d0-4d0*mion**2/spa)
 
-c      print*,rts,rtspa,dsqrt(an)*rts
-c      stop
-
-c      if(ion_em)then
       beta=dsqrt(1d0-4d0*mp**2/sp*an**2/az**2)
       betaa=dsqrt(1d0-4d0*mp**2/sp*an**2/az**2)
       spa=mp**2+mion**2+sp*az/2d0*(1d0+beta*betaa)*az/an
       rtspa=dsqrt(spa)
-c      print*,rtspa,rtsi
-c      endif
-
+      
 cccc  proton in +/- z direction  
       if(io.eq.1)then
 
@@ -56,18 +44,12 @@ cccc  proton in +/- z direction
       q(4,1)=rtspa/2d0-(mion**2-mp**2)/rtspa/2d0
       q(3,1)=dsqrt(q(4,1)**2-mp**2)
 
-c      print*,rtspa/2d0
-
       q(1,2)=0d0
       q(2,2)=0d0
       q(4,2)=rtspa/2d0+(mion**2-mp**2)/rtspa/2d0
       q(3,2)=-dsqrt(q(4,2)**2-mion**2)
 
 cccccccc     
-
-c      print*,q(4,2),q(3,2),rtspa/2d0
-
-c      stop
 
       else
 
@@ -87,8 +69,6 @@ c      stop
 
       if(io.eq.1)then
 
-c      print*,rtsnn
-
       q(1,1)=0d0
       q(2,1)=0d0
       q(4,1)=rtsnn/2d0
@@ -98,11 +78,6 @@ c      print*,rtsnn
       q(2,2)=0d0
       q(4,2)=an*rtsnn/2d0
       q(3,2)=-dsqrt(q(4,2)**2-mion**2)
-
-c      print*,'test',an,rts,q(4,1)/q(3,1),q(4,2)/q(3,2)
-c      print*,dsqrt((q(4,1)+q(4,2))**2-(q(3,1)+q(3,2))**2),an*rts
-c      print*,dsqrt(2d0*(q(4,1)*q(4,2)-q(3,1)*q(3,2)))
-c     &,dsqrt(4d0*(q(4,1)*q(4,2)))
 
       else
 
