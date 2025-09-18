db3aa80f14d25eb4396731e2a17c4a8f00c48c17
LucianHL <l.harland-lang@ucl.ac.uk>
2025-09-18 10:56:03 +0100

    Update pAboost.f

--- src/phase/pAboost.f
+++ src/phase/pAboost.f
@@ -29,8 +29,6 @@ ccc   boosts from nucleon-nucleon c.m.s to lab frame for pA collsions
       px(1)=0d0
       px(2)=0d0
 
-c      print*,rtspa,rts  51672.54  1600821.
-
       if(ion_inel)then
       if(io.eq.1)then
       px(3)=rtsnn/2d0*(beta-an*betaa)
@@ -49,43 +47,24 @@ cccc  proton in +/- z direction
       px(4)=rtspA/2d0*(1d0+az)
       endif
 
-c      print*,rts/2d0
-c      stop
-
-
       ein=dsqrt(px(4)**2-px(3)**2-px(2)**2-px(1)**2)
-
-c      print*,ein 2243137. 
 
       jmax=nup+2
       if(ion_inel.and.ion_incoh_type.eq.'inel')jmax=jmax-1
-c      jmax=jmax-1
-
-c      print*,jmax
 
       yx=0.5d0*dlog((q(4,5)+q(3,5))/(q(4,5)-q(3,5)))
-c      print*,yx
 
       do j=1,jmax
          do i=1,4
             pcm(i)=q(i,j)
-c            print*,i,j,q(i,j)
          enddo
-c         print*,j,q(4,j)**2-q(3,j)**2-q(2,j)**2-q(1,j)**2
          call boost(ein,px,pcm,pboo)
-c         print*,j
          do i=1,4
             q(i,j)=pboo(i)
-c            print*,j,pboo(i)
          enddo
       enddo
 
-
       yx=0.5d0*dlog((q(4,5)+q(3,5))/(q(4,5)-q(3,5)))
-
-c      print*,yx
-c      print*,''
-c      stop
 
       do k=1,2
          do j=1,4
@@ -132,14 +111,9 @@ ccc   boosts from nucleon-nucleon c.m.s to lab frame for pA collsions
       px(1)=0d0
       px(2)=0d0
 
-c      print*,rtspa,rts  51672.54  1600821.
-
       if(ion_inel)then
-c      print*,rtsnn
-c      stop
       if(io.eq.1)then
       px(3)=rtsnn/2d0*(beta-an*betaa)
-c      px(3)=rtsnn/2d0*(1d0-an)
       else
       px(3)=-rtsnn/2d0*(beta-an*betaa)
       endif
@@ -154,40 +128,22 @@ cccc  proton in +/- z direction
       px(4)=rtspA/2d0*(1d0+az)
       endif
 
-
-
       ein=dsqrt(px(4)**2-px(3)**2-px(2)**2-px(1)**2)
-
-c      print*,ein,rtsnn,rtsnn*dsqrt(an)
-c      stop
-
-c      print*,ein 2243137. 
 
       jmax=nup+2
       if(ion_inel.and.ion_incoh_type.eq.'inel')jmax=jmax-1
-c      jmax=jmax-1
-
-c      print*,jmax
-
 
       do j=18,18
          do i=1,4
             pcm(i)=q(i,j)
-c            print*,i,j,q(i,j)
          enddo
-c         print*,j,q(4,j)**2-q(3,j)**2-q(2,j)**2-q(1,j)**2
          call boost(ein,px,pcm,pboo)
-c         print*,j
          do i=1,4
             q(i,j)=pboo(i)
-c            print*,j,pboo(i)
          enddo
       enddo
 
-
       yx=0.5d0*dlog((q(4,5)+q(3,5))/(q(4,5)-q(3,5)))
-
-c      print*,yx
 
       do k=1,2
          do j=1,4
